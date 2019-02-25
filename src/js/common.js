9e5c092a0c01a01294fa7d8e09b5235632eb84e6
Hitomi Kitamura <hinitial@gmail.com>
2019-02-25 21:53:43 +0900

    Webpack 勉強中

--- src/js/common.js
+++ src/js/common.js
@@ -4,3 +4,5 @@ import isString from '@/lib/isString';
 
 console.log('isString',isString(123));
 console.log('isString',isString('hoge'));
+
+import '../sass/test.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,44 @@
-const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-	mode:'development',
 	devtool: 'source-map',
 	entry: ['./src/js/common.js'],
 	output: {
-		path: `${__dirname}/dist/js`,
-		filename: 'common.js'
+		path: `${__dirname}/dist`,
+		filename: 'js/common.js'
 	},
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, 'src/js'),
+			'@': `${__dirname}/src/js`,
 		},
-	}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/,
+				exclude: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader'
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].css',
+			chunkFilename: 'css/[id].css'
+		}),
+		new CopyWebpackPlugin(
+				[
+					{
+						from: 'img',
+						to: 'img',
+					},
+				],
+				{ context: 'src' } // from の基準
+		),
+	],
 };
